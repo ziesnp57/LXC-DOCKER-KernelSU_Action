b808212078ac65384e19fa0017bd42f67d4d0aa5
5kind <shikabane320@gmail.com>
2023-11-25 04:27:31 +0000

    disruptive change

--- scripts/custom.sh
+++
@@ -1,3 +0,0 @@
-CONFIG_FILE=$1
-scripts/config --file $CONFIG_FILE --enable CONFIG_CFS_BANDWIDTH
-scripts/config --file $CONFIG_FILE --set-str CONFIG_LOCALVERSION "-Marisa"
---
+++ scripts/customize.sh
@@ -0,0 +1,3 @@
+CONFIG_FILE=$1
+scripts/config --file $CONFIG_FILE --enable CONFIG_BINFMT_MISC
+scripts/config --file $CONFIG_FILE --set-str CONFIG_LOCALVERSION "-Marisa"
