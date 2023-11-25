6148b4706c13a16ce93c6d1e8763a9654ee2dc35
5kind <shikabane320@gmail.com>
2023-11-25 08:15:29 +0000

    new customize.sh

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -1,3 +1,4 @@
 CONFIG_FILE=$1
-scripts/config --file $CONFIG_FILE --enable CONFIG_BINFMT_MISC
-scripts/config --file $CONFIG_FILE --set-str CONFIG_LOCALVERSION "-Marisa"
+# scripts/config --file $CONFIG_FILE --enable CONFIG_BINFMT_MISC
+# scripts/config --file $CONFIG_FILE --set-str CONFIG_LOCALVERSION "-Marisa"
+$GITHUB_WORKSPACE/scripts/check-kernel-config/customize.sh $CONFIG_FILE -w
