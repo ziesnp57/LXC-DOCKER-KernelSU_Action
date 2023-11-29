8a0b47e0928612017f9022d8f3f730f83254efc7
5kind <shikabane320@gmail.com>
2023-11-29 01:26:00 +0000

    Docker patch

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -1,9 +1,4 @@
 CONFIG_FILE=$1
-# scripts/config --file $CONFIG_FILE --enable CONFIG_REFCOUNT_FULL
-# scripts/config --file $CONFIG_FILE --enable CONFIG_IP_VS_PROTO_RR
-# scripts/config --file $CONFIG_FILE --enable CONFIG_CFS_BANDWIDTH
-# scripts/config --file $CONFIG_FILE --enable CONFIG_AUFS_FS
-# scripts/config --file $CONFIG_FILE --enable CONFIG_DEVPTS_MULTIPLE_INSTANCES
-# scripts/config --file $CONFIG_FILE --enable CONFIG_BINFMT_MISC
-# scripts/config --file $CONFIG_FILE --set-str CONFIG_LOCALVERSION "-Marisa"
-$GITHUB_WORKSPACE/scripts/check-kernel-config/customize.sh $CONFIG_FILE -w
+scripts/config --file $CONFIG_FILE --enable CONFIG_BINFMT_MISC
+scripts/config --file $CONFIG_FILE --set-str CONFIG_LOCALVERSION "-Marisa"
+# $GITHUB_WORKSPACE/scripts/check-kernel-config/customize.sh $CONFIG_FILE -w
